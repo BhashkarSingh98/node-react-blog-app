095b253fe32ebaa3422c9a43345f43d766db1e86
bhashkar <bhashkar2600@gmail.com>
2022-10-15 22:11:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require("express")
+
+const app=express();
+
+const datarouter=require("./Routes/data");
+const cors=require("cors");
+app.use(cors());
+
+app.use("/api",datarouter);
+
+app.listen(process.env.PORT || 8080,()=>{
+    console.log("app is running");
+})
+ 
